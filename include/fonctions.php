8415f1a054f9180f1c9727b80078f4f168d1020d
Thomas BERARD <thomasberard@users.noreply.github.com>
2016-04-05 09:22:14 +0200

    Update fonction.php
    Modification du footer et commentation du code

--- include/fonctions.php
+++ include/fonctions.php
@@ -1,16 +1,22 @@
 <?php
-
+/*
+* \Autheur: Nicolas BOUYSSOUNOUSE.
+* \Vérificateur: Romain VINCENT.
+* \Brief: Vérifie si l'utilisateur est connecté.
+* \Return: Retourne 'true' si l'array global $_session contient la chaine de caractère 'ID'.
+*/
 function isConnected(){
-	// Author Nicolas
-	// Checker Romain
-	// Renvois 'true' si l'array global $_session contient la chaine de caractère 'ID'
 	return isset($_SESSION['ID']);
 }
 
+
+/*
+* \Autheur: Thomas BERARD.
+* \Vérificateur: Théo GUIBOUD-RIBAUD.
+* \Brief: Génération du haut de la page HTML.
+* \Return: <!DOCTYPE html> & co.
+*/
 function hautPage () {
-	/** Retourne le code HTML du haut de la page HTML.
-	Author = Thomas
-	Checker = Romain*/
 	return '<!DOCTYPE html>
 <html>
 <head>
@@ -27,18 +33,26 @@ function hautPage () {
 	';
 }
 
+
+/*
+* \Autheur: Thomas BERARD.
+* \Vérificateur: Théo GUIBOUD-RIBAUD.
+* \Brief: Génération du bas de la page HTML.
+* \Return: </body> & </html>.
+*/
 function basPage () {
-	/** Retourne le code HTML du bas de la page HTML.
-	Author = Thomas
-	Checker = Romain*/
 	return '</body>
 </html>';
 }
 
+
+/*
+* \Autheur: Thomas BERARD.
+* \Vérificateur: Théo GUIBOUD-RIBAUD.
+* \Brief: Affiche la barre de navigation en tête de page.
+* \Return: Barre de navigation.
+*/
 function afficheMenu () {
-	/** Retourne le code HTML de la navbar en tête de page.
-	Author = Thomas
-	Checker = Romain*/
 	return '<nav id="cssmenu">
 	<ul>
 	<li><a href="#">Accueil</a></li>
@@ -52,14 +66,31 @@ function afficheMenu () {
 ';
 }
 
+
+/*
+* \Autheur: Thomas BERARD.
+* \Vérificateur: 
+* \Brief: Affiche le footer en bas de page.
+* \Return: Footer complet.
+*/
 function afficheFooter () {
-	/** Retourne le code HTML du footer pour le bas de page.
-	Author = Thomas
-	Checker = Romain*/
-	return '<footer id="cssfooter">
-	<ul>
-	<li id="epsi-logo"><a href="http://www.epsi.fr" target="_blank"><img src="images/epsi-logo.png" width="100px"></a></li>
-</footer>
+	return '<footer class="footer-distributed">
+
+			<div class="footer-right">
+
+				<a href="https://www.facebook.com/Campus.EPSI.Grenoble" target="_blank"><img src="images/facebook-logo.png" alt="Facebook EPSI Grenoble" width="42" height="42" border="0"></a>
+				<a href="https://twitter.com/EPSIGrenoble" target="_blank"><img src="images/twitter-logo.png" alt="Twitter EPSI Grenoble" width="42" height="42" border="0"></a>
+
+			</div>
+
+			<div class="footer-left">
+
+				<a href="http://www.epsi.fr" target="_blank"><img src="images/epsi-logo.png" alt="Facebook EPSI Grenoble" width="120" border="0"></a>
+
+			</div>
+
+		</footer>
 ';
+}
 
 ?>
