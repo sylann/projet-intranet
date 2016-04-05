c35c4462feedadf1fd989dc00fdcabba6c70ff48
valsov <valentin.sovignet@epsi.fr>
2016-04-05 16:29:48 +0200

    re ajout fichier manquant

--- include/fonctions.php
+++ include/fonctions.php
@@ -139,5 +139,83 @@ function affichehumeur(){
 }
 
 
+function getUtilisateur($idUtilisateur){
+	global $p_base;			//pour avoir accès à la la variable $p_base
+	/**
+	\brief Retourner un tableau chargé des infos de l'utilisateur
+	\author = Valentin
+	\checker = ?
+	\param = idUtilisateur entier
+	\return un tableau*/
+	try{
+		$p_requete = $p_base->prepare("SELECT * FROM personne WHERE id = :idUtilisateur");		//requête SQL nous donnant toutes les informations d'un utilisateur
+		$p_requete->execute(array('idUtilisateur'=> $idUtilisateur));
+		$donnees = $p_requete->fetch();
+
+		$tableauUtilisateur["mail"] = $donnees['mail'];					//on met toutes les informations de la table dans le tableau
+		$tableauUtilisateur["password"] = $donnees['password'];
+		$tableauUtilisateur["nom"] = $donnees['nom'];
+		$tableauUtilisateur["prenom"] = $donnees['prenom'];
+		$tableauUtilisateur["statut"] = $donnees['statut'];
+		$tableauUtilisateur["pseudo"] = $donnees['pseudo'];
+		$tableauUtilisateur["datenaiss"] = $donnees['datenaiss'];
+		$tableauUtilisateur["tel"] = $donnees['tel'];
+		$tableauUtilisateur["telpublic"] = $donnees['telpublic'];
+		$tableauUtilisateur["photo"] = $donnees['photo'];
+		$tableauUtilisateur["avatar"] = $donnees['avatar'];
+		$tableauUtilisateur["cv"] = $donnees['cv'];
+		$tableauUtilisateur["cvpublic"] = $donnees['cvpublic'];
+		$tableauUtilisateur["devise"] = $donnees['devise'];
+		$tableauUtilisateur["signature"] = $donnees['signature'];
+		$tableauUtilisateur["acceptmails"] = $donnees['acceptmails'];
+		$tableauUtilisateur["nbpost"] = $donnees['nbpost'];
+		$tableauUtilisateur["nblikes"] = $donnees['nblikes'];
+		$tableauUtilisateur["nbarticles"] = $donnees['nbarticles'];
+		$tableauUtilisateur["nbcontribution"] = $donnees['nbcontribution'];
+
+		$p_requete->closeCursor(); 		// Termine le traitement de la requête
+	}
+	catch(Exception $e){
+	// En cas d'erreur précédemment, on affiche un message et on arrête tout
+		die('Erreur : '.$e->getMessage());
+	}
+	return $tableauUtilisateur;		//on retourne le tableau
+}
+
+function chargeUtilisateur($tableauUtilisateur){
+	/**
+	\brief mettre les informations de l'utilisateur dans les $_SESSION
+	\author = Valentin
+	\checker = ?
+	\param = tableauUtilisateur tableau
+	\return rien*/
+	foreach($tableauUtilisateur as $key => $value){		//pour chaque valeur dans le tableau
+		$_SESSION[$key] = $value;						//on assigne une valeur à cette variable de session
+	}
+}
+
+function afficheUtilisateur($tableauUtilisateur){
+	/**
+	\brief Renvoie les informations de l'utilisateur (sa fiche)
+	\author = Valentin
+	\checker = ?
+	\param = tableauUtilisateur tableau
+	\return string*/
+	$chaine = "";
+	foreach($tableauUtilisateur as $key => $value){				//pour chaque valeur dans le tableau
+		$chaine .= '<p>-- ' . $key . ' : ' . $value . '</p>';	//on fait des paragraphes avec ce qu'est la variable et sa valeur
+	};
+	return'<div>' . $chaine . '</div>';						//on return la chaine
+}
+
+function afficheMiniUtilisateur($tableauUtilisateur){
+	/**
+	\brief Renvoie les informations de l'utilisateur sous la forme d'une mini fiche
+	\author = Valentin
+	\checker = ?
+	\param = tableauUtilisateur tableau
+	\return string*/
+	return'<img src="' . $tableauUtilisateur['photo'] . '" title="Nom : ' . $tableauUtilisateur['nom'] . ', Prenom : ' . $tableauUtilisateur['prenom'] .'">';
+}
 
 ?>
