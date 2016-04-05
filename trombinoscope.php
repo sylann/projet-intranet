865e78a34f9f7d5049374a074e9a292af4a665dc
Wraithtorturer <jerome.fabbian@epsi.fr>
2016-04-05 10:52:20 +0200

    PageTrombinoscope

---
+++ trombinoscope.php
@@ -0,0 +1,28 @@
+<?php
+
+/** Retourne la page d'affichage du trombinoscope en étant connecté.
+	Author = Jérôme
+	Checker = */
+
+
+	include('include/fonctions.php');		// Appelle la liste des fonctions
+
+	if (!isConnected()) {					// Regarde si l'utilisateur est connecté
+		die('Page inaccessible');			// Si il ne l'est pas, ne charge pas la page
+	}	
+		  				
+	echo afficheHautPage ();  				// Appelle la fonction HautPage
+	echo afficheMenu ();      				// Appelle la fonction Menu
+	echo afficheChat ();  					// Si connecté, affiche le chat grâce à sa fonction
+
+	if ($_GET['groupe'] == '') {			// Si la valeur $_GET de groupe est vide
+		echo afficheGroupes ();				// Appelle la fonction afficheGroupes responsable d'afficher la liste des groupes
+	}
+	else {									// Sinon
+		echo afficheTrombinoscope ();		// Appelle la fonction afficheTrombinoscope
+	}
+
+	echo afficheFooter ();					// Appelle la fonction Footer
+	echo BasPage ();						// Appelle la fonction Baspage
+
+?>
