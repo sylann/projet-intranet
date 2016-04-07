4f9cd7c911bbec81ad8590032447d85e6d181eba
Romain Vincent <romainfvincent@gmail.com>
2016-04-07 10:50:07 +0200

    correction syntax + ajout fonctions blog
    Nettoyage et correction complète de la syntaxe doxygène.
    
    Ajout des 3 fonctions faites par adrien :
    getpost
    getlastblog
    getblog

--- include/fonctions.php
+++ include/fonctions.php
@@ -1,23 +1,23 @@
 <?php
 /**
-* \Message de Sylann :
-* \
-* \ Lorsque vous ouvrez ce fichier pour y ajouter votre fonction, faites bien attention à ne pas modifier le reste du fichier
-* \ Sauf si c'est nécessaire
-* \
-* \ La meilleur façon de faire est d'ajouter votre travail à la fin de ce fichier, en laissant deux
-* \ lignes vides entre la dernière accolade et la première ligne de votre commentaire entète.
-* \
-* \ Pour l'entète, dans le doute, en reprendre un existant.
-* \ Bon codage !
+* Message de Sylann :
+* 
+* Lorsque vous ouvrez ce fichier pour y ajouter votre fonction, faites bien attention à ne pas modifier le reste du fichier
+* Sauf si c'est nécessaire
+* 
+* La meilleur façon de faire est d'ajouter votre travail à la fin de ce fichier, en laissant deux
+* lignes vides entre la dernière accolade et la première ligne de votre commentaire entète.
+* 
+* Pour l'entète, dans le doute, en reprendre un existant.
+* Bon codage !
 */
 
 
 /**
-* \Autheur: Nicolas BOUYSSOUNOUSE.
-* \Vérificateur: Romain VINCENT.
-* \Brief: Vérifie si l'utilisateur est connecté.
-* \Return: Retourne 'true' si l'array global $_session contient la chaine de caractère 'ID'.
+*\author Nicolas BOUYSSOUNOUSE
+*\checker Romain VINCENT
+*\brief Vérifie si l'utilisateur est connecté
+*\return Retourne 'true' si l'array global $_session contient la chaine de caractère 'ID'
 */
 function isConnected(){
 	return isset($_SESSION['ID']);
@@ -25,10 +25,10 @@ function isConnected(){
 
 
 /**
-* \Autheur: Thomas BERARD.
-* \Vérificateur: Théo GUIBOUD-RIBAUD.
-* \Brief: Génération du haut de la page HTML.
-* \Return: <!DOCTYPE html> & co.
+*\author Thomas BERARD
+*\checker Théo GUIBOUD-RIBAUD
+*\brief Génération du haut de la page HTML
+*\return string
 */
 function hautPage () {
 	return '<!DOCTYPE html>
@@ -49,10 +49,10 @@ function hautPage () {
 
 
 /**
-* \Autheur: Thomas BERARD.
-* \Vérificateur: Théo GUIBOUD-RIBAUD.
-* \Brief: Génération du bas de la page HTML.
-* \Return: </body> & </html>.
+*\author Thomas BERARD
+*\checker Théo GUIBOUD-RIBAUD
+*\brief Génération du bas de la page HTML
+*\return string
 */
 function basPage () {
 	return '</body>
@@ -61,34 +61,34 @@ function basPage () {
 
 
 /**
-* \Autheur: Thomas BERARD.
-* \Vérificateur: Théo GUIBOUD-RIBAUD.
-* \Brief: Affiche la barre de navigation en tête de page.
-* \Return: Barre de navigation.
+*\author Thomas BERARD.
+*\checker Théo GUIBOUD-RIBAUD.
+*\brief Affiche la barre de navigation en tête de page.
+*\return string
 */
 function afficheMenu () {
 	return '<nav id="cssmenu">
-	<ul>
-	<li><a href="#">Accueil</a></li>
-	<li><a href="#">page1</a></li>
-	<li><a href="#">page2</a></li>
-	<li><a href="#">page3</a></li>
-	<li><a href="#">page4</a></li>
-	<li><a href="#">page5</a></li>
-	<li><input style="margin-top: 5px; margin-left: 15px; width: 150px; text-align: center" type="text" id="identifiant" name="identifiant" placeholder="Entrez votre identifiant"/></li>
-	<li><input style="margin-top: 5px; margin-left: 15px; width: 150px; text-align: center" type="password" id="password" name="password" placeholder="Entrez votre password"/></li>
-	<li><button type="submit">CONNEXION</button></li>
-	</ul>
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">page1</a></li>
+			<li><a href="#">page2</a></li>
+			<li><a href="#">page3</a></li>
+			<li><a href="#">page4</a></li>
+			<li><a href="#">page5</a></li>
+			<li><input style="margin-top: 5px; margin-left: 15px; width: 150px; text-align: center" type="text" id="identifiant" name="identifiant" placeholder="Entrez votre identifiant"/></li>
+			<li><input style="margin-top: 5px; margin-left: 15px; width: 150px; text-align: center" type="password" id="password" name="password" placeholder="Entrez votre password"/></li>
+			<li><button type="submit">CONNEXION</button></li>
+		</ul>
 	</nav>
 	';
 }
 
 
 /**
-* \Autheur: Thomas BERARD.
-* \Vérificateur: Jérôme FABBIAN.
-* \Brief: Affiche le footer en bas de page.
-* \Return: Footer complet.
+*\author Thomas BERARD
+*\checker Jérôme FABBIAN
+*\brief Affiche le footer en bas de page
+*\return string
 */
 function afficheFooter () {
 	return '<footer class="footer-distributed">
@@ -106,10 +106,10 @@ function afficheFooter () {
 
 
 /**
-* \Autheur: Florian GOJON.
-* \Vérificateur: Guillaume
-* \Brief: Génére un tableau avec les id des 25 derniers messages.
-* \Return: tableau.
+*\author Florian GOJON
+*\checker Nathan
+*\brief Génére un tableau avec les id des 25 derniers messages
+*\return array
 */
 function listeMessages() {
 	global $p_base;
@@ -122,10 +122,14 @@ function listeMessages() {
 }
 
 
-/**brief : fonction qui génère le div « humeur du jour »
-Entrée : Rien
-Sortie : Chaîne
-Formate une chaîne html qui affiche sélectionne une humeur au hasard dans la table et l'affiche*/
+/**
+*\author Guillaume
+*\checker 
+*\brief fonction qui génère le div « humeur du jour »
+*\return string
+* Information supplémentaire
+* Formate une chaîne html qui affiche sélectionne une humeur au hasard dans la table et l'affiche
+*/
 function affichehumeur(){
 	global $p_base;
 	try{
@@ -139,14 +143,15 @@ function affichehumeur(){
 }
 
 
+/**
+*\author Valentin
+*\checker
+*\brief Retourner un tableau chargé des infos de l'utilisateur
+*\param /a $idUtilisateur
+*\return array
+*/
 function getUtilisateur($idUtilisateur){
-	global $p_base;			//pour avoir accès à la la variable $p_base
-	/**
-	*\brief Retourner un tableau chargé des infos de l'utilisateur
-	*\author = Valentin
-	*\checker = Guillaume
-	*\param = idUtilisateur entier
-	*\return un tableau*/
+	global $p_base;
 	try{
 		$p_requete = $p_base->prepare("SELECT * FROM personne WHERE id = :idUtilisateur");		//requête SQL nous donnant toutes les informations d'un utilisateur
 		$p_requete->execute(array('idUtilisateur'=> $idUtilisateur));
@@ -183,25 +188,28 @@ function getUtilisateur($idUtilisateur){
 }
 
 
+/**
+*\author = Valentin
+*\checker = ?
+*\brief mettre les informations de l'utilisateur dans les  \a $_SESSION/
+*\param \a $tableauUtilisateur/
+*\return rien
+*/
 function chargeUtilisateur($tableauUtilisateur){
-	/**
-	*\brief mettre les informations de l'utilisateur dans les $_SESSION
-	*\author = Valentin
-	*\checker = ?
-	*\param = tableauUtilisateur tableau
-	*\return rien*/
 	foreach($tableauUtilisateur as $key => $value){		//pour chaque valeur dans le tableau
 		$_SESSION[$key] = $value;						//on assigne une valeur à cette variable de session
 	}
 }
 
+
+/**
+*\author Valentin
+*\checker
+*\brief Renvoie les informations de l'utilisateur (sa fiche)
+*\param \a $tableauUtilisateur/
+*\return string
+*/
 function afficheUtilisateur($tableauUtilisateur){
-	/**
-	*\brief Renvoie les informations de l'utilisateur (sa fiche)
-	*\author = Valentin
-	*\checker = ?
-	*\param = tableauUtilisateur tableau
-	*\return string*/
 	$chaine = "";
 	foreach($tableauUtilisateur as $key => $value){				//pour chaque valeur dans le tableau
 		$chaine .= '<p>-- ' . $key . ' : ' . $value . '</p>';	//on fait des paragraphes avec ce qu'est la variable et sa valeur
@@ -209,14 +217,116 @@ function afficheUtilisateur($tableauUtilisateur){
 	return'<div>' . $chaine . '</div>';						//on return la chaine
 }
 
+
+/**
+*\author Valentin
+*\checker
+*\brief Renvoie les informations de l'utilisateur sous la forme d'une mini fiche
+*\param \a $tableauUtilisateur/
+*\return string
+*/
 function afficheMiniUtilisateur($tableauUtilisateur){
-	/**
-	*\brief Renvoie les informations de l'utilisateur sous la forme d'une mini fiche
-	*\author = Valentin
-	*\checker = ?
-	*\param = tableauUtilisateur tableau
-	*\return string*/
 	return'<img src="' . $tableauUtilisateur['photo'] . '" title="Nom : ' . $tableauUtilisateur['nom'] . ', Prenom : ' . $tableauUtilisateur['prenom'] .'">';
 }
 
-?>
+
+/**
+*\author Adrien
+*\checker 
+*\brief Fonction affichage d'un post avec partie user, mise en page & toolbar
+*\param \a $idPost/
+*\return string
+* Autres informations :
+* Requète SQL avec recherche de la date & du contenu du post, de la signature, de l'avatar, du pseudo, du nom & du prenom du user
+* 'table' Affichage forum sous forme tableau, largeur 100%, class "posttable"
+* 'td user' Affichage du bloc user avec pseudo, devise & avatar, affichage centré, largeur 150px
+* 'td post'  Affichage du bloc post avec une toolbar en partie supérieure, le contenu du post & la signature du user, qui prends le reste de la place en largeur (100%-150px)
+* 'toolbar' Affichage d'une toolbar avec la date & (fonctions à venir)
+* 'idPost' Affichage du post et de la signature du user
+*/
+function affichePost ($idPost) {
+	try {
+		$p_requete = $p_base->query('Select date, contenu, idpersonne, signature, post.id, avatar, pseudo from post, personne, devise where post.id = '.$idPost' and idpersonne = personne.id');
+		$donnees = $requete->fetchall());
+	}
+	catch(Exception $e){
+	die ('Erreur : '.$e->getMessage());
+	}
+	return	'<table class="posttable" cellpadding="4" cellspacing="0" width="100%"> 
+				<tbody>
+					<td class="user" rowspan="1" valign="top" width="150">
+						<center>
+							<div>'.$donnees['pseudo'].'
+							</br>
+							<div>'.$donnees['devise'].'
+ 							<img src="'.$donnees['avatar'].'" title="'.$donnees['nom'].','. $donnees['prenom'].'">
+ 						</center>
+					</td>
+					<td class="post" valign="top">
+						<div class="toolbar">
+							Date du message : '.$donnees['date']'
+						</div>
+						<div class="'.$idPost.'">
+							<p>'.$donnees['contenu'].'</p>
+							</br>
+						</div>
+						<div class="signature">
+							--------------------
+							</br>'
+							.$donnees['signature'].'
+						</div>
+					</td>
+				</tbody>
+			</table>';
+}
+
+
+/**
+*\author Adrien
+*\checker Romain
+*\brief Fonction récupération des derniers blogs à afficher sur la page d'accueil du site, genère un tableau avec les id des 5 derniers blogs
+*\return array
+* Information supplémentaire
+* Nombre de blog à ramener actuellement = 5
+* Requète SQL avec recherche de l'id & de la date du post, l'id du topic, en vérifiant qu'il s'agit d'un blog, classement descendant par date, avec une limite de 5 blogs pour l'affichage principal
+* Rangement des résultats dans le tableau 'tableLastBlog'
+*/
+function getLastBlog() {
+	global $p_base;
+	try {
+		$requete = $p_base->query('select post.id as id, post.date, blog, topic.id, idtopic from post, topic where idtopic = topic.id and blog is TRUE order by post.date desc limit 5');
+		$tableLastBlog = array();
+		while($resultat = $requete->fetch()) {
+			$tableLastBlog[] = $resultat['id'];
+		}
+	}
+	catch(Exception $e){
+		die ('Erreur : '.$e->getMessage());
+	}
+	return $tableLastBlog;
+}
+
+
+/**
+*\author Adrien
+*\checker Romain
+*\brief Fonction récupération de tous les blogs à afficher sur la page d'archive des blogs, genère un tableau avec les id de tous les blogs
+*\return array
+* Information supplémentaire
+* Requète SQL avec recherche de l'id & de la date du post, l'id du topic, en vérifiant qu'il s'agit d'un blog, classement descendant par date pour l'affichage des archives
+* Rangement des résultats dans le tableau 'tableBlog'
+*/
+function getBlog() {
+	global $p_base;
+	try {
+		$requete = $p_base->query('select post.id as id, post.date, blog, topic.id, idtopic from post, topic where idtopic = topic.id and blog is TRUE order by post.date desc');
+		$tableBlog = array();
+		while($resultat = $requete->fetch()) {
+			$tableBlog[] = $resultat['id'];
+		}
+	}
+	catch(Exception $e){
+	die ('Erreur : '.$e->getMessage());
+	}
+	return $tableBlog;
+}
