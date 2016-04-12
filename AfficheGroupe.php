18bed16ec04d450233075bec2ca3525d32859820
Romain Vincent <romainfvincent@gmail.com>
2016-04-12 23:52:43 +0200

    retrait indentation superflue

--- AfficheGroupe.php
+++ AfficheGroupe.php
@@ -6,8 +6,8 @@
 * \return Une chaine html pour afficher les groupes 
 */
 
-	function AfficheGroupe() {
-		/*On retourne une chaine de caractère html qui renvoie les groupes et les liens vers les différentes pages*/
-	return	<p><a href="trombinoscope.php?groupe=promo2020"Promo 2020 </a> </br> <a href="trombinoscope.php?groupe=promo2019">Promo 2019</a> </br> <a href="trombinoscope.php?groupe=promo2018">Promo 2018</a> </br> <a href="trombinoscope.php?groupe=promo2017">Promo 2017</a> </br> <a href="trombinoscope.php?groupe=promo2016">Promo 2016</a> </br> <a href="trombinoscope.php?groupe=intervenant">Intervenants </a></br> <a href="trombinoscope.php?groupe=administration">Administration </a></p>	
-	}
-	?>
+function AfficheGroupe() {
+	/*On retourne une chaine de caractère html qui renvoie les groupes et les liens vers les différentes pages*/
+return	<p><a href="trombinoscope.php?groupe=promo2020"Promo 2020 </a> </br> <a href="trombinoscope.php?groupe=promo2019">Promo 2019</a> </br> <a href="trombinoscope.php?groupe=promo2018">Promo 2018</a> </br> <a href="trombinoscope.php?groupe=promo2017">Promo 2017</a> </br> <a href="trombinoscope.php?groupe=promo2016">Promo 2016</a> </br> <a href="trombinoscope.php?groupe=intervenant">Intervenants </a></br> <a href="trombinoscope.php?groupe=administration">Administration </a></p>	
+}
+?>
