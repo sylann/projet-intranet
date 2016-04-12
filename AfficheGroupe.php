cb0712f80d4300a552bf4eae9f5055b2974c66dc
Romain Vincent <romainfvincent@gmail.com>
2016-04-13 00:04:02 +0200

    Vérificaction fonction affiche groupe

--- AfficheGroupe.php
+++ AfficheGroupe.php
@@ -1,13 +1,23 @@
 <?php 
 /**
 * \author Théo GUIBOUD-RIBAUD
-* \verificator 
-* \brief :Affiche la liste des groupes d'utilisateurs
-* \return Une chaine html pour afficher les groupes 
+* \checker Romain VINCENT
+* \brief Affiche la liste des groupes d'utilisateurs
+* \return String
 */
 
 function AfficheGroupe() {
-	/*On retourne une chaine de caractère html qui renvoie les groupes et les liens vers les différentes pages*/
-return	<p><a href="trombinoscope.php?groupe=promo2020"Promo 2020 </a> </br> <a href="trombinoscope.php?groupe=promo2019">Promo 2019</a> </br> <a href="trombinoscope.php?groupe=promo2018">Promo 2018</a> </br> <a href="trombinoscope.php?groupe=promo2017">Promo 2017</a> </br> <a href="trombinoscope.php?groupe=promo2016">Promo 2016</a> </br> <a href="trombinoscope.php?groupe=intervenant">Intervenants </a></br> <a href="trombinoscope.php?groupe=administration">Administration </a></p>	
+	/* Retourne une chaine de caractère html
+	* liste les groupes et contient des liens vers chaque page correspodante
+	*/
+	$html = "<p>\n";
+	$html .= "<a href=\"trombinoscope.php?groupe=promo2019\">Promo 2019</a>\n";
+	$html .= "<a href=\"trombinoscope.php?groupe=promo2018\">Promo 2018</a>\n";
+	$html .= "<a href=\"trombinoscope.php?groupe=promo2017\">Promo 2017</a>\n";
+	$html .= "<a href=\"trombinoscope.php?groupe=promo2016\">Promo 2016</a>\n";
+	$html .= "<a href=\"trombinoscope.php?groupe=intervenant\">Intervenants</a>\n";
+	$html .= "<a href=\"trombinoscope.php?groupe=administration\">Administration</a>\n";
+	$html .= "</p>";
+	return $html;
 }
 ?>
