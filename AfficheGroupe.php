f6392f7d654242c54612d21c3267afea93a35a61
Rascarat <ttheo38@gmail.com>
2016-04-12 10:26:43 +0200

    AfficheGroupe Théo

---
+++ AfficheGroupe.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+* \author Théo GUIBOUD-RIBAUD
+* \verificator 
+* \brief :Affiche la liste des groupes d'utilisateurs
+* \return Une chaine html pour afficher les groupes 
+*/
+
+	function AfficheGroupe() {
+		/*On retourne une chaine de caractère html qui renvoie les groupes et les liens vers les différentes pages*/
+	return	<p><a href="trombinoscope.php?groupe=promo2020"Promo 2020 </a> </br> <a href="trombinoscope.php?groupe=promo2019">Promo 2019</a> </br> <a href="trombinoscope.php?groupe=promo2018">Promo 2018</a> </br> <a href="trombinoscope.php?groupe=promo2017">Promo 2017</a> </br> <a href="trombinoscope.php?groupe=promo2016">Promo 2016</a> </br> <a href="trombinoscope.php?groupe=intervenant">Intervenants </a></br> <a href="trombinoscope.php?groupe=administration">Administration </a></p>	
+	}
+	?>
