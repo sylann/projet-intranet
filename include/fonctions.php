86910870d7eb8e3ffc4a6606058e98c9a85a5b5c
Adrien Ceccaldi <adrien.ceccaldi@hotmail.fr>
2016-04-13 08:30:32 +0200

    getDernierArticle
    Ajout getDernierArticle

--- include/fonctions.php
+++ include/fonctions.php
@@ -445,3 +445,28 @@ function afficheFaitJour(){
     $alea = rand(0, countMOTD()-1);
     return faitJour($alea);
 }
+
+/**
+*\author Adrien
+*\checker 
+*\brief Fonction retourne l'id du dernier article
+*\return id article
+* Informations supplémentaires
+* Nombre d'article à ramener actuellement = 1
+* Return l'Id d'un article, cet article est choisi par requête SQL, cette dernière est un select dans les articles où la date de création de l'article est la plus récente (order by date desc	et on prend le premier).
+* Rangement des résultats dans le tableau 'tableDernierArticle'
+*/
+function getDernierArticle() {
+	global $p_base;
+	try {
+		$requete = $p_base->query(' select id, datecrea from wiki order by datecrea DESC limit 1 ');
+		$tableDernierArticle = array();
+		while($resultat = $requete->fetch()) {
+			$tableDernierArticle[] = $resultat['id'];
+		}
+	}
+	catch(Exception $e){
+		die ('Erreur : '.$e->getMessage());
+	}
+	return $tableDernierArticle;
+}
