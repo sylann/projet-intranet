0dff42629297814e093502b8dc73e53a49dcfb06
Thomas BERARD <thomas.chabeuil@gmail.com>
2016-09-29 08:46:57 +0200

    ajout de la constance CHEMIN_SERVER

--- include/connexion-example.php
+++ include/connexion-example.php
@@ -10,6 +10,8 @@
 	define('DB_USER', "le_nom_de_votre_utilisateur"); #on défini l'utilisateur
 	define('DB_PASSWORD', "votre_mot_de_passe"); #on défini le mot de passe
 
+	define('CHEMIN_SERVER', "/var/www/grepsi.fr/www/"); #on défini le chemin absolu du serveur web
+
 	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 	$p_base = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_BASE, DB_USER, DB_PASSWORD, $pdo_options); #on créé une connexion avec la base de données
 	$p_base->exec("Set character set utf8"); #on convertit en utf8
