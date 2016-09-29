0208cc85b8ad38c78d2dd290e18363e1a2f43c2f
valsov <valentin.sovignet@sfr.fr>
2016-09-29 08:20:28 +0200

    Ajout doc fonction afficheGroupes
    J'avais oublié la doxygen de la fonction afficheGroupes dans fonctions.php

--- include/fonctions.php
+++ include/fonctions.php
@@ -698,6 +698,12 @@ function afficheArticle($idArticle){
 }
 
 
+/**
+*\author Valentin
+*\checker ?
+*\brief Renvoie une chaine html qui permet d'afficher les groupes avec un lien vers leur trombinoscope
+*\param rien
+*\return chaine html*/
 function afficheGroupes(){
 	global $p_base;			//pour avoir accès à la la variable $p_base
 	$groupes = '<div class="container-all-groups">';
